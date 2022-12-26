179b3114a9288ff8f092669496f48d3fef672e8e
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2022-12-26 21:53:36 +0900

    build setting and testing

--- src/main/java/com/programmers/java/baseball/Main.java
+++ src/main/java/com/programmers/java/baseball/Main.java
@@ -2,11 +2,14 @@ package com.programmers.java.baseball;
 
 import com.github.javafaker.Faker;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        Faker faker = new Faker();
-        // Random값 생성
-        String title = faker.name().title();
-        System.out.println(title);
+        User user = new User(24, "김승우");
+        User user2 = new User(24, "김승우");
+
+        System.out.println(user.equals(user2));
     }
 }
