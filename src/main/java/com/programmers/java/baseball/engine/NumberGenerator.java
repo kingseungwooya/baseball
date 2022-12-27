1f9d1e8ad1a8f57011e511ad6b27411f882d4eef
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2022-12-27 10:47:21 +0900

    refactor: remove dependency of main logic

--- src/main/java/com/programmers/java/baseball/engine/NumberGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.programmers.java.baseball.engine;
-
-import com.programmers.java.baseball.model.Numbers;
-
-public class NumberGenerator {
-    public Numbers generate(int count) {
-        return null;
-    }
-}
---
+++ src/main/java/com/programmers/java/baseball/io/NumberGenerator.java
@@ -0,0 +1,7 @@
+package com.programmers.java.baseball.io;
+
+import com.programmers.java.baseball.model.Numbers;
+
+public interface NumberGenerator {
+    public Numbers generate(int count);
+}
