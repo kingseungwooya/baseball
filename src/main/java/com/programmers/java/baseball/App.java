3333c81d9b14a7dbdb5adf5d76e24597d42d5083
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2022-12-27 11:36:41 +0900

    feat: create runnner

--- src/main/java/com/programmers/java/baseball/App.java
+++ src/main/java/com/programmers/java/baseball/App.java
@@ -1,7 +1,16 @@
 package com.programmers.java.baseball;
 
+import com.programmers.java.baseball.engine.BaseBall;
+import com.programmers.java.baseball.io.Input;
+import com.programmers.java.baseball.io.NumberGenerator;
+import com.programmers.java.baseball.io.Output;
+import com.programmers.java.baseball.model.Numbers;
+
 public class App {
     public static void main(String[] args) {
 
+        NumberGenerator numberGenerator = new FakerNumberGenerator();
+        Console console = new Console(); // input, output으로도 사용 가능
+        new BaseBall(numberGenerator, console, console).run();
     }
 }
