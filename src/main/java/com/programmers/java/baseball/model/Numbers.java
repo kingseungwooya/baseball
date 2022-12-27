a1f8c373abdb6ca42118cb4b83d03408cec41f09
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2022-12-27 10:47:29 +0900

    feat: add getter

--- src/main/java/com/programmers/java/baseball/model/Numbers.java
+++ src/main/java/com/programmers/java/baseball/model/Numbers.java
@@ -1,8 +1,10 @@
 package com.programmers.java.baseball.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class Numbers {
     private Integer[] nums;
 }
